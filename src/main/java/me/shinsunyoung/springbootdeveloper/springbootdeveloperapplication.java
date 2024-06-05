bcff508859e0147a8f960b384bf02f53c4e5f57f
Hwannee00 <doormoo2@naver.com>
2024-06-05 17:15:31 +0900

    start spring

---
+++ src/main/java/me/shinsunyoung/springbootdeveloper/springbootdeveloperapplication.java
@@ -0,0 +1,12 @@
+package me.shinsunyoung.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+@SpringBootApplication
+public class springbootdeveloperapplication {
+    public static void main(String[] args){
+        SpringApplication.run(springbootdeveloperapplication.class, args);
+    }
+}
